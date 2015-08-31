679633212a648dba2cf3eda8af465d8612ffaa72
Leo Lincourt <leolincourt@gmail.com>
2015-08-31 10:15:35 -0400

    Project setup

---
+++ utils/fetch-data.r
@@ -0,0 +1,6 @@
+# Fetch NLIHC OOR data
+# Requires datasets package for state data
+# See: https://stat.ethz.ch/R-manual/R-devel/library/datasets/html/state.html
+# URL template: http://nlihc.org/sites/default/files/oor/files/reports/state/2015-OOR-AL.xls
+
+
