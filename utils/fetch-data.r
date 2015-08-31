e81bb184811b21cfc3b0bc1090e4a65814859984
Leo Lincourt <leolincourt@gmail.com>
2015-08-31 12:55:04 -0400

    Require plyr

--- utils/fetch-data.r
+++ utils/fetch-data.r
@@ -5,6 +5,13 @@
 # See: https://stat.ethz.ch/R-manual/R-devel/library/datasets/html/state.html
 require("datasets")
 
+# Requires plyr package for split-apply-combine goodness
+# See http://plyr.had.co.nz
+require("plyr")
+
+# Note: Useful url
+# https://books.google.com/books?id=X-7sBQAAQBAJ&pg=PA226&lpg=PA226&dq=r+download+list+of+files&source=bl&ots=ORmOUpNNVV&sig=38CJHKqc5IS5ZPUX-OfgTqUqHNg&hl=en&sa=X&ved=0CFgQ6AEwCGoVChMIlpOKyuLTxwIVgkiSCh0IngBX#v=onepage&q=r%20download%20list%20of%20files&f=false
+
 # Some variables to make life easier
 base_url <- "http://nlihc.org/sites/default/files/oor/files/reports/state/"
 oor_template <- "OOR"
