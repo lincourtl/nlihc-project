7c063df58ce51ce6487ade87e868fee2dc2a0c1d
Leo Lincourt <leolincourt@gmail.com>
2015-08-31 10:19:51 -0400

    Set up

--- utils/fetch-data.r
+++ utils/fetch-data.r
@@ -2,5 +2,5 @@
 # Requires datasets package for state data
 # See: https://stat.ethz.ch/R-manual/R-devel/library/datasets/html/state.html
 # URL template: http://nlihc.org/sites/default/files/oor/files/reports/state/2015-OOR-AL.xls
-
+#
 
