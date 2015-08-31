15d562efa82099354e88a6194eeb17fd51f9ec07
Leo Lincourt <leolincourt@gmail.com>
2015-08-31 11:54:36 -0400

    Clean up formatting again. Damn OCD.

--- utils/fetch-data.r
+++ utils/fetch-data.r
@@ -7,11 +7,10 @@ require("datasets")
 base_url <- "http://nlihc.org/sites/default/files/oor/files/reports/state/"
 oor_template <- "OOR"
 file_ext <- ".xls"
-data_urls <- paste( 
-              base_url,
-              paste( format(Sys.Date(),"%Y"), 
-                     oor_template,
-                     state.abb, 
-                     sep="-" ), 
-              file_ext, 
-              sep="" )
+data_urls <- paste( base_url, 
+                    paste( format(Sys.Date(),"%Y"), 
+                           oor_template,
+                           state.abb, 
+                           sep="-" ), 
+                    file_ext, 
+                    sep="" )
